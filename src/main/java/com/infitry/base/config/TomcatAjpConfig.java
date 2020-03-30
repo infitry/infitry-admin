5faab613c99b762dbc6d7d85ee10b183c1b9366a
leesw <leesw@DESKTOP-UT2THB4>
2020-03-30 14:13:25 +0900

    [2020-03-30] - AJP 설정

---
+++ src/main/java/com/infitry/base/config/TomcatAjpConfig.java
@@ -0,0 +1,27 @@
+package com.infitry.base.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatAjpConfig {
+
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(createAjpConnector());
+		return tomcat;
+	}
+
+	private Connector createAjpConnector() {
+		Connector ajpConnector = new Connector("AJP/1.3");
+		ajpConnector.setPort(18002);
+		ajpConnector.setSecure(false);
+		ajpConnector.setAllowTrace(false);
+		ajpConnector.setScheme("http");
+		return ajpConnector;
+	}
+}
